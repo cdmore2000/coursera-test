ae64f72dc9a45384b51cff5bbdf17af57d97d364
cdmore2000 <68013153+cdmore2000@users.noreply.github.com>
2020-07-31 19:19:26 +0530

    Add files via upload

---
+++ Assign_4_sol/script.js
@@ -0,0 +1,11 @@
+(function() {
+    var names = ["Ram", "John", "Jen", "Jason", "Sham", "Raj", "Raman", "Pandit", "Latina", "Jim"];
+    for (var i = 0; i < names.length; i++) {
+        var firstLetter = names[i].charAt(0).toLowerCase();
+        if (firstLetter === 'j') {
+            byeSpeaker.speak(names[i]);
+        } else {
+            helloSpeaker.speak(names[i]);
+        }
+    }
+})();
